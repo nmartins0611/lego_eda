1f19dd3b84a15ac4963fce37f54ca3d32eadd931
nmartins0611 <62961138+nmartins0611@users.noreply.github.com>
2023-01-27 18:01:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from aiokafka import AIOKafkaProducer
 from buildhat import ColorSensor
 import asyncio
+import json
+
+#colourData = {}
+#colourData["color"] = c
+#jsonData = json.dumps(colourData)
 
 async def send_one():
     producer = AIOKafkaProducer(bootstrap_servers='kafka.prometheus.io:9092')
@@ -11,9 +16,13 @@ async def send_one():
     try:
         while True:
             c = color.wait_for_new_color()
+            colourData = {}
+            colourData["color"] = c
+            jsonData = json.dumps(colourData)
 
-            # Produce message
-            await producer.send_and_wait("network", bytes(c, encoding='utf-8'))
+      # Produce message
+ #           await producer.send_and_wait("network", bytes(c, encoding='utf-8'))
+            await producer.send_and_wait("network", bytes(jsonData, encoding='utf-8'))
     finally:
         # Wait for all pending messages to be delivered or expire.
         await producer.stop()
